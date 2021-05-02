845675d6f31d02b30ce9be5b43ef97b6e6843fc3
Ubuntu <ubuntu@ip-172-31-60-217.ec2.internal>
2021-05-02 15:56:26 +0000

    Confirm working systems

--- AirLib/src/vehicles/multirotor/api/MultirotorApiBase.cpp
+++ AirLib/src/vehicles/multirotor/api/MultirotorApiBase.cpp
@@ -545,6 +545,10 @@ void MultirotorApiBase::moveByRC(const RCData& rc_data)
     throw VehicleCommandNotImplementedException("moveByRC API is not implemented for this multirotor");
 }
 
+void MultirotorApiBase::updateCommunicationsAbilities()
+{
+
+}
 
 void MultirotorApiBase::moveByVelocityInternal(float vx, float vy, float vz, const YawMode& yaw_mode)
 {
